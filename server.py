a9a863a98a6c13355286e7094daa4f0f6e5daca0
송지웅 <thdwldnd7@dgu.ac.kr>
2024-12-21 02:06:38 +0900

    fix: 반환 형식을 json response로 변경

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 
 from fastapi import FastAPI
+from starlette.responses import JSONResponse
 
 from dto import UserConsultRequest
 from services import llm_service
@@ -8,17 +9,21 @@ app = FastAPI()
 
 @app.post("/medicine-consult")
 async def medicine_consult(req: UserConsultRequest):
-    return await llm_service.consult_drug_safety(req.chat_user_id, req.target_id, req.concern)
+    content = await llm_service.consult_drug_safety(req.chat_user_id, req.target_id, req.concern)
+    return JSONResponse(content={"response": content})
 
 @app.post("/medical-department-consult")
 async def department_consult(req: UserConsultRequest):
-    return await llm_service.consult_medical_department(req.chat_user_id, req.target_id, req.concern)
+    content = await llm_service.consult_medical_department(req.chat_user_id, req.target_id, req.concern)
+    return JSONResponse(content={"response": content})
 
 @app.post("/symptoms-and-guidance-consult")
 async def symptoms_and_guidance_consult(req: UserConsultRequest):
-    return await llm_service.consult_symptoms_and_guidance(req.chat_user_id, req.target_id, req.concern)
+    content = await llm_service.consult_symptoms_and_guidance(req.chat_user_id, req.target_id, req.concern)
+    return JSONResponse(content={"response": content})
 
 @app.post("/general-consult")
 async def general_consult(req: UserConsultRequest):
-    return await llm_service.general_consult(req.chat_user_id, req.target_id, req.concern)
+    content = await llm_service.general_consult(req.chat_user_id, req.target_id, req.concern)
+    return JSONResponse(content={"response": content})
 
