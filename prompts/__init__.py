55adb0dbc3271a24485588db9a5b487dd8af3a23
송지웅 <thdwldnd7@dgu.ac.kr>
2024-12-21 23:23:08 +0900

    fix: prompt 변경

--- prompts/__init__.py
+++ prompts/__init__.py
@@ -1,5 +1,6 @@
-from prompts import recommend_subject, drug_safety, router, general_consult, symptoms_guidance
+from prompts import recommend_subject, drug_safety, router, general_consult, symptoms_guidance, base
 
+base_prompt = base.prompt
 recommend_subject_prompt = recommend_subject.prompt
 drug_safety_prompt = drug_safety.prompt
 symptoms_guidance_prompt = symptoms_guidance.prompt
